09815398baba5053413c3e2fead636b527695a79
a1778478 <a1778478@f124924c-131e-4e9b-aee0-b9ab63c2b873>
2021-06-09 06:49:14 +0000

    try if this works

--- routing/DistanceVector/DistanceVector.py
+++ routing/DistanceVector/DistanceVector.py
@@ -1,2 +1,52 @@
 #!/usr/bin/env python3
-print ("hello world!")
+print ( '''router X at t=0
+ 	Y	Z
+ Y	2	INF
+ Z	INF	7
+ 
+ router Y at t=0
+ 	X	Z
+ X	2	INF
+ Z	INF	1
+ 
+ router Z at t=0
+ 	X	Y
+ X	7	INF
+ Y	INF	1
+ 
+ router X at t=1
+ 	Y	Z
+ Y	2	8
+ Z	3	7
+ 
+ router Y at t=1
+ 	X	Z
+ X	2	8
+ Z	9	1
+ 
+ router Z at t=1
+ 	X	Y
+ X	7	3
+ Y	9	1
+ 
+ router X at t=2
+ 	Y	Z
+ Y	2	8
+ Z	3	7
+ 
+ router Y at t=2
+ 	X	Z
+ X	2	4
+ Z	5	1
+ 
+ router Z at t=2
+ 	X	Y
+ X	7	3
+ Y	9	1
+ 
+ router X: Y is 2 routing through Y
+ router X: Z is 3 routing through Y
+ router Y: X is 2 routing through X
+ router Y: Z is 1 routing through Z
+ router Z: X is 3 routing through Y
+ router Z: Y is 1 routing through Y''')
