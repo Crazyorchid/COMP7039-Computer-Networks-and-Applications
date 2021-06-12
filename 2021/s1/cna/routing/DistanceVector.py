365b670ef52a9ad3cd1b55cd0d59cc6ada813082
a1778478 <a1778478@f124924c-131e-4e9b-aee0-b9ab63c2b873>
2021-06-12 05:33:48 +0000

    update

--- 2021/s1/cna/routing/DistanceVector.py
+++ 2021/s1/cna/routing/DistanceVector.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python3
 print("here")
 def DistanceVector():
-    last = ''
+    last = '   '
     messageList = []
     while True:
         router = input()
         print("Test:", router)
         messageList.append(router)
-        if last == '\n' and router == '\n':
+        if last == '' and router == '':
             break
         last = router
+    print("sdfs")
     for item in messageList:
         print(item)
 
